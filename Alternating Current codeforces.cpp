4511cefc2617ed60138b0c623a718814fd0c6220
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-02-27 12:10:56 +0530

    1

---
+++ Alternating Current codeforces.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    stack<char> st;
+    for(int i=0;i<s.length();i++){
+        if(!st.empty()&&st.top()==s[i]){
+            st.pop();
+        }else{
+            st.push(s[i]);
+        }
+    }
+    if(!st.empty()){
+        cout<<"No";
+    }else{
+        cout<<"Yes";
+    }
+}
