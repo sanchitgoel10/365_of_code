9b61e6e34458b07a35d696278feaaf9fcc1b03e0
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-04-27 03:12:00 +0530

    Ok

---
+++ Bike tour kickstart.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define mii map<int,int>
+#define set set<int>
+#define vec vector<int>
+#define pq priority_queue <int>
+#define pqr priority_queue <int, vector<int>, greater<int>>
+#define mod 1000000007
+#define pi 3.1415926535897932384626433832795
+#define inf 1e18
+#define ff first
+#define ss second
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define w(t) int t;cin>>t;while(t--)
+
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+
+int32_t main(){
+	FastRead;
+	int tc=1;
+	w(t){
+	    int n;
+	    cin>>n;
+	    int*arr =  new int[n];
+	    For(i,0,n)cin>>arr[i];
+	    int ans=0;
+	    For(i,1,n-1){
+	        if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])ans++;
+	    }
+	    cout<<"Case"<<" "<<"#"<<tc<<":"<<" "<<ans<<endl;
+        tc++;
+	}
+	return 0;
+}
+
