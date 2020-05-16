8111d363b4018b34e18bc8a95bc0997d4b2e547c
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-05-17 04:13:32 +0530

    Nice Board

---
+++ Leetcode Interview/Alphabet Board Path.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        unordered_map<char, pair<int,int>> mp;
+        for(int i = 0; i < 26; ++i) {
+            mp[i+'a'] = {i/5, i%5};
+        }
+        target = 'a' + target;
+        string path;
+        int dx = 0, dy = 0;
+        for(int i = 1; i < target.size(); ++i) {
+            auto cur = mp[target[i]];
+            auto prev = mp[target[i-1]];
+            dx = cur.first - prev.first;
+            dy = cur.second - prev.second;
+            if(dy < 0) path += string(-dy, 'L');
+            if(dx < 0) path += string(-dx, 'U');
+            if(dy > 0) path += string(dy, 'R');
+            if(dx > 0) path += string(dx, 'D');
+            path += '!';
+        }
+        return path;
+    }
+};
