f43cc93b947c5f9b288224db3212544414170ef4
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-05-26 20:15:00 +0530

    10 linked list ka khatarnak cycle ka concept

---
+++ Leetcode Interview/142. Linked List Cycle II.cpp
@@ -0,0 +1,40 @@
+//Concept hai gaurav sen ki video dekhio 
+/*
+Consider the following linked list, where E is the cylce entry and X, the crossing point of fast and slow.
+        H: distance from head to cycle entry E
+        D: distance from E to X
+        L: cycle length
+                          _____
+                         /     \
+        head_____H______E       \
+                        \       /
+                         X_____/   
+        
+    
+        If fast and slow both start at head, when fast catches slow, slow has traveled H+D and fast 2(H+D). 
+        Assume fast has traveled n loops in the cycle, we have:
+        2H + 2D = H + D + L  -->  H + D = nL  --> H = nL - D
+        Thus if two pointers start from head and X, respectively, one first reaches E, the other also reaches E. 
+        In my solution, since fast starts at head.next, we need to move slow one step forward in the beginning of part 2
+*/
+ListNode *detectCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    
+    ListNode *slow  = head;
+    ListNode *fast  = head;
+    ListNode *entry = head;
+    
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {                      // there is a cycle
+            while(slow != entry) {               // found the entry location
+                slow  = slow->next;
+                entry = entry->next;
+            }
+            return entry;
+        }
+    }
+    return NULL;                                 // there has no cycle
+}
