15cd207f9060af65ad8d83442a13cdaecc027866
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-05-29 19:31:25 +0530

    Day 1

---
+++ Average/Day 1/328. Odd Even Linked List.cpp
@@ -0,0 +1,14 @@
+ ListNode* oddEvenList(ListNode* head) 
+    {
+        if(!head) return head;
+        ListNode *odd=head, *evenhead=head->next, *even = evenhead;
+        while(even && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
+    }
