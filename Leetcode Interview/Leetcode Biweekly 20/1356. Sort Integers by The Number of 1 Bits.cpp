e26e389968b6431815c4cd3115bb1c90e629d2e4
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-06-17 20:26:08 +0530

    7

--- Leetcode Interview/Leetcode Biweekly 20/1356. Sort Integers by The Number of 1 Bits.cpp
+++ Leetcode Interview/Leetcode Biweekly 20/1356. Sort Integers by The Number of 1 Bits.cpp
@@ -10,3 +10,10 @@ public:
         return arr;
     }
 };
+//Anther way of writing cmp function withing sort function
+vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(),[](const int& a, const int& b){
+            int countA = __builtin_popcount(a), countB = __builtin_popcount(b);
+            return countA==countB ? a<b:countA<countB;});
+        return arr;
+    }
--- Template.cpp
+++ Template.cpp
@@ -19,12 +19,12 @@ using namespace std;
 #define For(i,a,b) for(int i=a;i<b;i++)
 #define w(t) int t;cin>>t;while(t--)
 
-//const int fx[]={+1,-1,+0,+0};
-//const int fy[]={+0,+0,+1,-1};
-//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
-//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
-//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
-//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+//const int fx[4]={+1,-1,+0,+0};
+//const int fy[4]={+0,+0,+1,-1};
+//const int fx[8]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[8]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[8]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[8]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
 
 // class node{
 //     public:
