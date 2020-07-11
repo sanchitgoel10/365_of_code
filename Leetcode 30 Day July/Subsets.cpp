ff869492cf908caa9226d2e027bbb5cc2a788167
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-07-11 17:10:48 +0530

    9 bitmasking

---
+++ Leetcode 30 Day July/Subsets.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        int p=1<<n;
+        vector<vector<int>> subs(p);
+        for(int i=0;i<p;i++){
+            for(int j=0;j<n;j++){
+                if(i>>j&1)subs[i].push_back(nums[j]);         //bitmasking
+            }
+        }
+        return subs;
+    }
+};
