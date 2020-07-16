5d6cec68b091ddd2271cfc23758c24e24e2175fd
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-07-16 18:56:02 +0530

    8

---
+++ Leetcode 30 Day July/Pow(x, n).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n==0) return 1;
+        double t = pow(x,n/2);
+        if (n%2) {
+            return n<0 ? 1/x*t*t : x*t*t;
+        } else {
+            return t*t;
+        }
+    }
+};
