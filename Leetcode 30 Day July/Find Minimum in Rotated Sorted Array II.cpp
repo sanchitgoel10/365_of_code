652f0338e7cc800e5b417d589fab325094bbf923
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-07-26 07:02:51 +0530

    10 nice imp

---
+++ Leetcode 30 Day July/Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]>nums[h]){
+                l=mid+1;
+            }else if(nums[mid]<nums[h]){
+                h--;
+            }else{
+                if(nums[h-1]>nums[h]){
+                    l=h;
+                    break;
+                }
+                h--;
+            }
+        }
+        return nums[l];
+    }
+};
