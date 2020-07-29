c9c8b3d1c5cc77cd72cbac063f412094a26e3355
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-07-29 09:46:36 +0530

    10

---
+++ Leetcode Interview/673. Number of Longest Increasing Subsequence.cpp
@@ -0,0 +1,27 @@
+/*
+The idea is to use two arrays len[n] and cnt[n] to record the maximum length of Increasing Subsequence and the coresponding number of these sequence which ends with nums[i], respectively. That is:
+
+len[i]: the length of the Longest Increasing Subsequence which ends with nums[i].
+cnt[i]: the number of the Longest Increasing Subsequence which ends with nums[i].
+
+Then, the result is the sum of each cnt[i] while its corresponding len[i] is the maximum length.
+*/
+
+int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size(), res = 0, max_len = 0;
+        vector<pair<int,int>> dp(n,{1,1});            //dp[i]: {length, number of LIS which ends with nums[i]}
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j <i ; j++){
+                if(nums[i] > nums[j]){
+                    if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
+                    if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
+                }
+            }
+            if(max_len == dp[i].first)res += dp[i].second;
+            if(max_len < dp[i].first){
+                max_len = dp[i].first;
+                res = dp[i].second;
+            }
+        }
+        return res;
+    }
