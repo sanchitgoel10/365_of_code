2af2c519f6b9da49c7127f051c9d74ecdcf625eb
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-07-30 13:57:38 +0530

    Useless

---
+++ Leetcode 30 Day July/Word Break II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    unordered_map<string, vector<string>> m;
+
+    vector<string> combine(string word, vector<string> prev){
+        for(int i=0;i<prev.size();++i){
+            prev[i]+=" "+word;
+        }
+        return prev;
+    }
+
+public:
+    vector<string> wordBreak(string s, vector<string>& dict) {
+        if(m.count(s)) return m[s]; //take from memory
+        vector<string> result;
+        if(find(dict.begin(),dict.end(),s)!=dict.end()){ //a whole string is a word
+            result.push_back(s);
+        }
+        for(int i=1;i<s.size();++i){
+            string word=s.substr(i);
+            if(find(dict.begin(),dict.end(),word)!=dict.end()){
+                string rem=s.substr(0,i);
+                vector<string> prev=combine(word,wordBreak(rem,dict));
+                result.insert(result.end(),prev.begin(), prev.end());
+            }
+        }
+        m[s]=result; //memorize
+        return result;
+    }
+};
