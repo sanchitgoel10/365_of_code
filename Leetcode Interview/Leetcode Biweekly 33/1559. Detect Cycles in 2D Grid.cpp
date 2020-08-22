715ca3fb522bd4a837a75e4a5ac24bb2dbf61bc9
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-08-22 23:51:56 +0530

    9.5

---
+++ Leetcode Interview/Leetcode Biweekly 33/1559. Detect Cycles in 2D Grid.cpp
@@ -0,0 +1,34 @@
+/*
+At each point (i, j) in grid, do following:
+
+Start DFS from (i, j).
+Don't visit the last visited point as stated in question.
+Only visit a point if it has same character as starting position
+If you still reach a visited point again, there is a cycle.
+*/
+class Solution {
+public:
+    vector<int> dir = { 0, 1, 0, -1, 0 }; 
+    bool isCyclic(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int lastx, int lasty)
+    {
+        visited[i][j] = true;
+        for(int d = 0; d < 4; ++d)
+        {
+            int a = i+dir[d];
+            int b = j+dir[d+1];
+            if(a >= 0 && a < grid.size() && b >= 0 && b < grid[0].size() && grid[a][b] == grid[i][j] && !(lastx == a && lasty == b))
+                if(visited[a][b] || isCyclic(grid, visited, a,b,i,j))
+                    return true;
+        }
+        return false;
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                if(!visited[i][j] && isCyclic(grid, visited, i, j, -1, -1))
+                    return true;
+        return false;
+    }
+};
