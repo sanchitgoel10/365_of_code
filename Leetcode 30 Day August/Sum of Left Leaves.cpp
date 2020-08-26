0d417f07c6e65302c092ac6b9c5265a6fa075710
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-08-26 14:49:19 +0530

    8

---
+++ Leetcode 30 Day August/Sum of Left Leaves.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root,bool isleft=false) {
+        if (!root) return 0;
+        if (!root->left && !root->right) return isleft ? root->val : 0;
+        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+    }
+};
