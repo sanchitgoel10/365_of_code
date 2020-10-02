8e57bb5274c30a7d11359a6e4f9d358524894fbc
sanchitgoel10 <sanchitgoel537@gmail.com>
2020-10-02 19:06:21 +0530

    7

---
+++ Leetcode Interview/Leetcode Biweekly 34/1572. Matrix Diagonal Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+                ans+=mat[i][i];
+                ans+=mat[i][n-i-1];
+        }
+        if(n%2){
+            ans-=mat[n/2][n/2];
+        }
+        return ans;
+    }
+};
